e544ccbd9134212b20a5561217e0264d94cb6b05
Anshumaan Singh <anshumaanborntolead@gmail.com>
2024-03-02 18:24:28 +0530

    Update function_overriding.java

--- Polymorphism/function_overriding.java
+++ Polymorphism/function_overriding.java
@@ -1,5 +1,26 @@
 package Polymorphism;
-
+class claculator{
+    public void adder(int x,int y){
+        int s=x+y;
+        System.out.println(s+" Not Overidden"); 
+    }
+    public void subtractor(int x,int y){
+        int d=y-x;
+        System.out.println(d+" Not Overidden as no function available in subclass to override"); 
+    }
+}
+class advancedcalculator extends claculator{
+    public void adder(int x,int y){
+        int s=x+y+1;
+        System.out.println(s+" Overidden"); 
+    }
+}
 public class function_overriding {
-    
+    public static void main(String[] args) {
+        int a=2;
+        int b=3;
+        advancedcalculator cl=new advancedcalculator();
+        cl.subtractor(a, b);
+        cl.adder(a, b);
+    }
 }
